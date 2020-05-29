c0919165089d58560679d6c7f99cd705d66a2ff0
Maxim Reznik <reznikmm@gmail.com>
2020-05-29 13:50:59 +0300

    Add bintray action

---
+++ index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+const path = require('path');
+const fs = require('fs');
+const glob = require('@actions/glob');
+const base64 = require('base-64');
+
+(async () => {
+    try {
+        const repository = core.getInput('repository');
+        const package = core.getInput('package');
+        const version = core.getInput('version');
+        const username = core.getInput('username');
+        const override = core.getInput('override');
+        const apiKey = core.getInput('apiKey');
+        const sourcePath = core.getInput('sourcePath');
+
+        const globber = await glob.create(sourcePath);
+
+        for await (const file of globber.globGenerator()) {
+            const basename = path.basename(file);
+            const url = 'https://api.bintray.com/' +
+                `/content/${username}/${repository}/${basename}`;
+            const size = fs.statSync(file).size;
+            const options = {
+                method: 'PUT',
+                body: fs.createReadStream(file),
+                headers: {
+                    'Authorization': 'Basic ' + base64.encode (`${username}:${apiKey}`),
+                    'Content-Type': 'application/octet-stream',
+                    'Content-Length': size,
+                    'X-Bintray-Package': package,
+                    'X-Bintray-Version': version,
+                    'X-Bintray-Publish': 1,
+                    'X-Bintray-Override': override
+                }
+            };
+            const response = await fetch(url, options);
+
+            if (response.ok) {
+                console.log(`Uploaded ${basename}.`);
+            } else {
+                console.log('Error:', response);
+                core.setFailed('Upload failed');
+            };
+        };
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+})();
