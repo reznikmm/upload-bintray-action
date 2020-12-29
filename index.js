b6bf41c9b9122ff89721fccae0f78a3f3a2d7a7e
Miro Kropacek <miro.kropacek@gmail.com>
2020-12-29 13:07:35 +0100

    Add destinationPath parameter

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const base64 = require('base-64');
         const override = core.getInput('override');
         const apiKey = core.getInput('apiKey');
         const sourcePath = core.getInput('sourcePath');
+        const destinationPath = core.getInput('destinationPath');
 
         if (!apiKey){
             core.warning('apiKey was not set. Skipping the upload.');
@@ -42,9 +43,9 @@ const base64 = require('base-64');
         const globber = await glob.create(sourcePath);
 
         for await (const file of globber.globGenerator()) {
-            const basename = path.basename(file);
+            const bintrayPath = destinationPath + '/' + path.basename(file);
             const url = 'https://api.bintray.com/' +
-                `/content/${subject}/${repository}/${basename}`;
+                `/content/${subject}/${repository}/${bintrayPath}`;
             const size = fs.statSync(file).size;
             const options = {
                 method: 'PUT',
@@ -62,7 +63,7 @@ const base64 = require('base-64');
             const response = await fetch(url, options);
 
             if (response.ok) {
-                console.log(`Uploaded ${basename}.`);
+                console.log(`Uploaded ${bintrayPath}.`);
             } else {
                 console.log('Error:', response);
                 core.setFailed('Upload failed');
